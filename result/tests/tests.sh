e114c6ea6d1ee4523a491e28d3455e878692b4c1
Bret Fisher <bret@bretfisher.com>
2019-04-14 19:30:53 -0400

    gave result some swarm/k8s friendly features

--- result/tests/tests.sh
+++ result/tests/tests.sh
@@ -7,14 +7,14 @@ done
 curl -sS -X POST --data "vote=b" http://vote > /dev/null
 sleep 10
 
-if phantomjs render.js http://result | grep -q '1 vote'; then
-  echo -e "\\e[42m------------"
-  echo -e "\\e[92mTests passed"
-  echo -e "\\e[42m------------"
-  exit 0
-else
+if phantomjs ./tests/render.js http://localhost | grep -q '0 vote'; then
   echo -e "\\e[41m------------"
   echo -e "\\e[91mTests failed"
   echo -e "\\e[41m------------"
   exit 1
+else
+  echo -e "\\e[42m------------"
+  echo -e "\\e[92mTests passed"
+  echo -e "\\e[42m------------"
+  exit 0
 fi
